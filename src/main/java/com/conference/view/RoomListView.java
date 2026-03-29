5b40c71cabe3e4d8f552e4bfb59fdd0f94d06491
Bacher100 <bacher.smaali@iteam-univ.tn>
2026-03-29 16:36:30 +0200

    Add RoomListView.java for the conference room reservation system

---
+++ src/main/java/com/conference/view/RoomListView.java
@@ -0,0 +1,13 @@
+package com.conference.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RoomListView {
+    @FXML
+    private Button addRoomButton;
+    @FXML
+    private Button removeRoomButton;
+    // Add other required components and methods here
+}
